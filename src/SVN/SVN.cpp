stefankueng/TortoiseSVN

commit 209524b004d47463f0c5473be0781e48bf0acbb5

Don't access the log cache pool for non-log-related functionality * load the log cache pool only on demand   (first log cache pool access)
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@14534 cc8e0003-5e82-43e2-9771-63201f7d8d9c

src/SVN/SVN.cpp
@@ -41,6 +41,7 @@
 #include "CacheLogQuery.h"
 #include "RepositoryInfo.h"
 #include "MessageBox.h"
+#include "LogCacheSettings.h"
 #include "..\version.h"
 
 
@@ -76,7 +77,7 @@ SVN::SVN(void) : m_progressWnd(0)
 	, progress_total(0)
 	, progress_lastprogress(0)
 	, progress_lasttotal(0)
-	, logCachePool (*this, CPathUtils::GetAppDataDirectory()+_T("logcache\\"))
+	, logCachePool()
 {
 	parentpool = svn_pool_create(NULL);
 	svn_error_clear(svn_client_create_context(&m_pctx, parentpool));
@@ -140,7 +141,9 @@ SVN::~SVN(void)
 {
 	svn_error_clear(Err);
 	svn_pool_destroy (parentpool);
-	logCachePool.Flush();
+
+    if (logCachePool.get() != NULL)
+	    logCachePool->Flush();
 }
 
 CString SVN::CheckConfigFile()
@@ -1349,12 +1352,12 @@ bool SVN::DiffSummarizePeg(const CTSVNPath& path, const SVNRev& peg, const SVNRe
 
 LogCache::CCachedLogInfo* SVN::GetLogCache (const CTSVNPath& path)
 {
-	if (!logCachePool.IsEnabled())
+    if (LogCache::CSettings::GetEnabled())
         return NULL;
 
     CString uuid;
-    CString root = logCachePool.GetRepositoryInfo().GetRepositoryRootAndUUID (path, uuid);
-    return logCachePool.GetCache (uuid, root);
+    CString root = GetLogCachePool()->GetRepositoryInfo().GetRepositoryRootAndUUID (path, uuid);
+    return GetLogCachePool()->GetCache (uuid, root);
 }
 
 BOOL SVN::ReceiveLog(const CTSVNPathList& pathlist, const SVNRev& revisionPeg, const SVNRev& revisionStart, const SVNRev& revisionEnd, int limit, BOOL strict, BOOL withMerges, bool refresh)
@@ -1366,10 +1369,10 @@ BOOL SVN::ReceiveLog(const CTSVNPathList& pathlist, const SVNRev& revisionPeg, c
 		SVNPool localpool(pool);
 
         CSVNLogQuery svnQuery (m_pctx, localpool);
-		CCacheLogQuery cacheQuery (&logCachePool, &svnQuery);
+		CCacheLogQuery cacheQuery (GetLogCachePool(), &svnQuery);
 		CCacheLogQuery refreshQuery (*this, &svnQuery);
 
-		ILogQuery* query = logCachePool.IsEnabled()
+		ILogQuery* query = logCachePool->IsEnabled()
 						 ? refresh ? static_cast<ILogQuery*>(&refreshQuery)
                                    : static_cast<ILogQuery*>(&cacheQuery)
 						 : static_cast<ILogQuery*>(&svnQuery);
@@ -1387,13 +1390,13 @@ BOOL SVN::ReceiveLog(const CTSVNPathList& pathlist, const SVNRev& revisionPeg, c
                    , false
                    , TRevPropNames());
 
-        if (refresh && logCachePool.IsEnabled())
+        if (refresh && logCachePool->IsEnabled())
         {
             // handle cache refresh results
 
             if (refreshQuery.GotAnyData())
             {
-                refreshQuery.UpdateCache (&logCachePool);
+                refreshQuery.UpdateCache (GetLogCachePool());
             }
             else
             {
@@ -1986,7 +1989,7 @@ CString SVN::GetRepositoryRoot(const CTSVNPath& url)
 	
     // use cached information, if allowed
 
-	if (logCachePool.IsEnabled())
+	if (LogCache::CSettings::GetEnabled())
     {
         // look up in cached repository properties
         // (missing entries will be added automatically)
@@ -1994,7 +1997,7 @@ CString SVN::GetRepositoryRoot(const CTSVNPath& url)
         CTSVNPath canonicalURL;
         canonicalURL.SetFromSVN (goodurl);
 
-        CRepositoryInfo& cachedProperties = logCachePool.GetRepositoryInfo();
+        CRepositoryInfo& cachedProperties = GetLogCachePool()->GetRepositoryInfo();
 
         CString result = cachedProperties.GetRepositoryRoot (canonicalURL);
         if (result.IsEmpty())
@@ -2120,7 +2123,7 @@ BOOL SVN::GetRootAndHead(const CTSVNPath& path, CTSVNPath& url, svn_revnum_t& re
 
     // use cached information, if allowed
 
-	if (logCachePool.IsEnabled())
+	if (LogCache::CSettings::GetEnabled())
     {
         // look up in cached repository properties
         // (missing entries will be added automatically)
@@ -2128,7 +2131,7 @@ BOOL SVN::GetRootAndHead(const CTSVNPath& path, CTSVNPath& url, svn_revnum_t& re
         CTSVNPath canonicalURL;
         canonicalURL.SetFromSVN (urla);
 
-        CRepositoryInfo& cachedProperties = logCachePool.GetRepositoryInfo();
+        CRepositoryInfo& cachedProperties = GetLogCachePool()->GetRepositoryInfo();
         CString uuid;
         url.SetFromSVN (cachedProperties.GetRepositoryRootAndUUID (path, uuid));
         if (url.IsEmpty())
@@ -2512,6 +2515,21 @@ CString SVN::GetOptionsString(BOOL bIgnoreEOL, svn_diff_file_ignore_space_t spac
 	}
 	opts.Trim();
 	return opts;
+}
+
+/**
+ * Returns the log cache pool singleton. You will need that to 
+ * create \c CCacheLogQuery instances.
+ */
+LogCache::CLogCachePool* SVN::GetLogCachePool() 
+{
+    if (logCachePool.get() == NULL)
+    {
+        CString cacheFolder = CPathUtils::GetAppDataDirectory()+_T("logcache\\");
+        logCachePool.reset (new LogCache::CLogCachePool (*this, cacheFolder));
+    }
+
+    return logCachePool.get();
 }
 
 /**

src/SVN/SVN.h
@@ -851,7 +851,7 @@ public:
 	 * Returns the log cache pool singleton. You will need that to 
 	 * create \c CCacheLogQuery instances.
 	 */
-	LogCache::CLogCachePool* GetLogCachePool() {return &logCachePool;}
+	LogCache::CLogCachePool* GetLogCachePool();
 
 	svn_error_t *				Err;			///< Global error object struct
 	svn_client_ctx_t * 			m_pctx;			///< pointer to client context
@@ -927,9 +927,11 @@ protected:
 	DWORD		progress_lastTicks;
 	std::vector<apr_off_t> progress_vector;
 
+private:
+
 	// the logCachePool must not be static: it uses apr/svn pools, and static objects
 	// will get cleaned up *after* we shut down apr.
-	LogCache::CLogCachePool logCachePool;
+    std::auto_ptr<LogCache::CLogCachePool> logCachePool;
 };
 
 static UINT WM_SVNPROGRESS = RegisterWindowMessage(_T("TORTOISESVN_SVNPROGRESS_MSG"));

src/TortoiseProc/LogDlg.cpp
@@ -47,6 +47,7 @@
 #include "CachedLogInfo.h"
 #include "RepositoryInfo.h"
 #include "EditPropertiesDlg.h"
+#include "LogCacheSettings.h"
 
 
 #if (NTDDI_VERSION < NTDDI_LONGHORN)
@@ -791,7 +792,7 @@ void CLogDlg::Refresh (bool autoGoOnline)
     if (autoGoOnline)
     {
 	    SetDlgTitle (false);
-        logCachePool.GetRepositoryInfo().ResetHeadRevision (m_sUUID, m_sRepositoryRoot);
+        GetLogCachePool()->GetRepositoryInfo().ResetHeadRevision (m_sUUID, m_sRepositoryRoot);
     }
 
 	InterlockedExchange(&m_bThreadRunning, TRUE);
@@ -1064,8 +1065,8 @@ UINT CLogDlg::LogThread()
     m_sURL = m_path.GetSVNPathString();
 
     // we need the UUID to unambigously identify the log cache
-    if (logCachePool.IsEnabled())
-        m_sUUID = logCachePool.GetRepositoryInfo().GetRepositoryUUID (rootpath);
+    if (LogCache::CSettings::GetEnabled())
+        m_sUUID = GetLogCachePool()->GetRepositoryInfo().GetRepositoryUUID (rootpath);
 
     // if the log dialog is started from a working copy, we need to turn that
     // local path into an url here
@@ -1214,7 +1215,7 @@ UINT CLogDlg::LogThread()
 	DialogEnableWindow(IDC_STATBUTTON, TRUE);
 	DialogEnableWindow(IDC_REFRESH, TRUE);
 
-	LogCache::CRepositoryInfo& cachedProperties = logCachePool.GetRepositoryInfo();
+	LogCache::CRepositoryInfo& cachedProperties = GetLogCachePool()->GetRepositoryInfo();
 	SetDlgTitle(cachedProperties.IsOffline (m_sUUID, m_sRepositoryRoot, false));
 
 	GetDlgItem(IDC_PROGRESS)->ShowWindow(FALSE);

commit 2cb101638dbbea658bf07a04350a85b130c19fdc

log cache did not get updated when modifying author or message http://svn.haxx.se/tsvn/archive-2009-02/0041.shtml (bug was introduced in r14534)
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@15300 cc8e0003-5e82-43e2-9771-63201f7d8d9c

src/SVN/SVN.cpp
@@ -1358,7 +1358,7 @@ bool SVN::DiffSummarizePeg(const CTSVNPath& path, const SVNRev& peg, const SVNRe
 
 LogCache::CCachedLogInfo* SVN::GetLogCache (const CTSVNPath& path)
 {
-    if (LogCache::CSettings::GetEnabled())
+    if (!LogCache::CSettings::GetEnabled())
         return NULL;
 
     CString uuid;