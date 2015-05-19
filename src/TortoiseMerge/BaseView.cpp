stefankueng/TortoiseSVN

commit f9a98a265800e88a334e4d1cc3ae509d22766d3c

For the inline diff, reverse the diff parts for the right view to get a diff consistent with the main diff.
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26468 cc8e0003-5e82-43e2-9771-63201f7d8d9c

src/TortoiseMerge/BaseView.cpp
@@ -4557,7 +4557,9 @@ bool CBaseView::GetInlineDiffPositions(int nViewLine, std::vector<inlineDiffPos>
         return false;
 
     svn_diff_t * diff = NULL;
-    m_svnlinediff.Diff(&diff, sLine, sLine.GetLength(), sDiffLine, sDiffLine.GetLength(), m_bInlineWordDiff);
+    auto pLine1 = (this == m_pwndLeft) ? &sLine : &sDiffLine;
+    auto pLine2 = (this == m_pwndLeft) ? &sDiffLine : &sLine;
+    m_svnlinediff.Diff(&diff, *pLine1, pLine1->GetLength(), *pLine2, pLine2->GetLength(), m_bInlineWordDiff);
     if (!diff || !SVNLineDiff::ShowInlineDiff(diff))
         return false;
 
@@ -4570,7 +4572,7 @@ bool CBaseView::GetInlineDiffPositions(int nViewLine, std::vector<inlineDiffPos>
 
         for (apr_off_t i = 0; i < len; ++i)
         {
-            position += m_svnlinediff.m_line1tokens[lineoffset].size();
+            position += (this == m_pwndRight) ? m_svnlinediff.m_line2tokens[lineoffset].size() : m_svnlinediff.m_line1tokens[lineoffset].size();
             lineoffset++;
         }
 
@@ -4838,7 +4840,9 @@ LineColors & CBaseView::GetLineColors(int nViewLine)
         svn_diff_t * diff = NULL;
         if (sLine.GetLength() > (int)m_nInlineDiffMaxLineLength)
             break;
-        m_svnlinediff.Diff(&diff, sLine, sLine.GetLength(), sDiffLine, sDiffLine.GetLength(), m_bInlineWordDiff);
+        auto pLine1 = (this == m_pwndLeft) ? &sLine : &sDiffLine;
+        auto pLine2 = (this == m_pwndLeft) ? &sDiffLine : &sLine;
+        m_svnlinediff.Diff(&diff, *pLine1, pLine1->GetLength(), *pLine2, pLine2->GetLength(), m_bInlineWordDiff);
         if (!diff || !SVNLineDiff::ShowInlineDiff(diff) || !diff->next)
             break;
 
@@ -4847,16 +4851,25 @@ LineColors & CBaseView::GetLineColors(int nViewLine)
         std::map<int, COLORREF> removedPositions;
         while (diff)
         {
+            if (this == m_pwndRight)
+            {
+                apr_off_t nTmp = diff->modified_length;
+                diff->modified_length = diff->original_length;
+                diff->original_length = nTmp;
+
+                nTmp = diff->modified_start;
+                diff->modified_start = diff->original_start;
+                diff->original_start = nTmp;
+            }
             apr_off_t len = diff->original_length;
 
-            CString s;
+            size_t nTextLength = 0;
             for (int i = 0; i < len; ++i)
             {
-                s += m_svnlinediff.m_line1tokens[lineoffset].c_str();
+                nTextLength += (this == m_pwndRight) ? m_svnlinediff.m_line2tokens[lineoffset].size() : m_svnlinediff.m_line1tokens[lineoffset].size();
                 lineoffset++;
             }
             bool bInlineDiff = (diff->type == svn_diff__type_diff_modified);
-            int nTextLength = s.GetLength();
 
             CDiffColors::GetInstance().GetColors(diffState, crBkgnd, crText);
             if ((m_bShowInlineDiff)&&(bInlineDiff))
@@ -4874,7 +4887,7 @@ LineColors & CBaseView::GetLineColors(int nViewLine)
             }
             oLineColors.SetColor(nTextStartOffset, crText, crBkgnd);
 
-            nTextStartOffset += nTextLength;
+            nTextStartOffset += (int)nTextLength;
             diff = diff->next;
         }
         for (std::map<int, COLORREF>::const_iterator it = removedPositions.begin(); it != removedPositions.end(); ++it)

commit 117a189f2cae8cc444e215efbb4b3e78d58c97d4

Fix an assertion introduced in r26468
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26524 cc8e0003-5e82-43e2-9771-63201f7d8d9c

src/TortoiseMerge/BaseView.cpp
@@ -4567,6 +4567,17 @@ bool CBaseView::GetInlineDiffPositions(int nViewLine, std::vector<inlineDiffPos>
     size_t position = 0;
     while (diff)
     {
+        if (this == m_pwndRight)
+        {
+            apr_off_t nTmp = diff->modified_length;
+            diff->modified_length = diff->original_length;
+            diff->original_length = nTmp;
+
+            nTmp = diff->modified_start;
+            diff->modified_start = diff->original_start;
+            diff->original_start = nTmp;
+        }
+
         apr_off_t len = diff->original_length;
         size_t oldpos = position;
 